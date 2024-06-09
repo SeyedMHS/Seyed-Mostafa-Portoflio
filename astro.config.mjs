b727dfb6b7c32652d6874369a6874163bb90fb72
Seyed Mostafa Hashemi Shahroudi <164580798+SeyedMHS@users.noreply.github.com>
2024-06-10 01:20:43 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.mostafa-hashemi.com",
+  site: "https://seyed-mostafa-portofliofdgfdgfdgd.vercel.app/",
   assets: true,
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
    
