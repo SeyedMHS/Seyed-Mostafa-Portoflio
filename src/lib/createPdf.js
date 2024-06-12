532f2d6765061688fdb7d84f47da33efc172477d
SeyedMHS <164580798+SeyedMHS@users.noreply.github.com>
2024-06-12 03:38:44 +0330

    new

---
+++ src/lib/createPdf.js
@@ -0,0 +1,16 @@
+import { PDFDocument, rgb } from "pdf-lib";
+export async function createPdf() {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage([600, 400]);
+    const { width, height } = page.getSize();
+  
+    page.drawText('Hello, this is a PDF document!', {
+      x: 50,
+      y: height / 2 + 200,
+      size: 30,
+      color: rgb(0, 0.53, 0.71),
+    });
+  
+    const pdfBytes = await pdfDoc.save();
+    return pdfBytes;
+  }
