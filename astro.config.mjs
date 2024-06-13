337bad6e39d3d246a0c9c15ec70e331a72d1ff4c
SeyedMHS <164580798+SeyedMHS@users.noreply.github.com>
2024-06-13 21:47:16 +0330

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
-import react from 'react'
 import react from '@astrojs/react'
 
 
--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+
 import react from '@astrojs/react'
 const DownloadButton = () => {
   return (
