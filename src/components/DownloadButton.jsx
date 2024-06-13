8041b4ea2812352936350750a73d0e48b81629cd
SeyedMHS <164580798+SeyedMHS@users.noreply.github.com>
2024-06-13 21:38:21 +0330

    update

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,5 +1,4 @@
 
-
 const DownloadButton = () => {
   return (
     <a href="../public/seyedmostafa-cv.pdf" class=' py-2 px-4 rounded truncate text-xs md:text-sm lg:text-base bg-black dark:bg-white text-white dark:text-black hover:opacity-75 blend' download>
