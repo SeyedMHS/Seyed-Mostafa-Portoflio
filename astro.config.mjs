11e0db0ddedca6340b7134b351b00cbf8da1b962
SeyedMHS <164580798+SeyedMHS@users.noreply.github.com>
2024-06-13 21:44:32 +0330

    تحديث

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import react from 'react'
+import react from '@astrojs/react'
+
+
 
 
 // https://astro.build/config
--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,4 +1,5 @@
-
+import react from 'react'
+import react from '@astrojs/react'
 const DownloadButton = () => {
   return (
     <a href="../public/seyedmostafa-cv.pdf" class=' py-2 px-4 rounded truncate text-xs md:text-sm lg:text-base bg-black dark:bg-white text-white dark:text-black hover:opacity-75 blend' download>
